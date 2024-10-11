39e69330a1b874b027b0ca083c6fae524e6f398a
shiponirfan <shiponirfan.dev@gmail.com>
2024-10-11 22:55:15 +0600

    Task Counter Section Added

--- lib/ui/screens/views/new_task_screen.dart
+++ lib/ui/screens/views/new_task_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager_app/ui/screens/views/add_new_task.dart';
+import 'package:task_manager_app/ui/widgets/task_summary_card_widget.dart';
 import 'package:task_manager_app/utils/app_colors.dart';
 
 class NewTaskScreen extends StatefulWidget {
@@ -14,21 +15,55 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: _buildFloatingActionButton(context),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [_buildTaskCount()],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTaskCount() {
+    return const SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          TaskSummaryCardWidget(
+            count: 09,
+            taskName: 'New',
+          ),
+          TaskSummaryCardWidget(
+            count: 03,
+            taskName: 'Progress',
+          ),
+          TaskSummaryCardWidget(
+            count: 04,
+            taskName: 'Completed',
+          ),
+          TaskSummaryCardWidget(
+            count: 01,
+            taskName: 'Canceled',
+          ),
+        ],
+      ),
     );
   }
 
   FloatingActionButton _buildFloatingActionButton(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const AddNewTask(),
-            ));
-      },
+      onPressed: _onTapFAB,
       backgroundColor: AppColors.primaryColor,
       foregroundColor: Colors.white,
       child: const Icon(Icons.add),
     );
+  }
+
+  void _onTapFAB() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const AddNewTask(),
+        ));
   }
 }
---
+++ lib/ui/widgets/task_summary_card_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class TaskSummaryCardWidget extends StatelessWidget {
+  const TaskSummaryCardWidget({
+    super.key,
+    required this.count,
+    required this.taskName,
+  });
+
+  final int count;
+  final String taskName;
+
+  @override
+  Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+    return Card(
+      elevation: 0,
+      shape: ContinuousRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      color: Colors.white,
+      child: SizedBox(
+        width: 90,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                count.toString(),
+                style: textTheme.titleLarge?.copyWith(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              FittedBox(
+                child: Text(
+                  taskName,
+                  style: const TextStyle(
+                      color: Colors.black54, fontWeight: FontWeight.w600),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
